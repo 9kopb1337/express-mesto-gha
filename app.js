ae81d4ea5d8829ed58dad811c185d016b339f5f8
Nikita <OvchinnikovNE1995@yandex.ru>
2023-06-15 22:51:00 +0300

    fixed id

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: '647f6834ca5b012549193198'
+    _id: '648b6b448bba69f325420a77'
   }
   next();
 })
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(router);
-app.use('/', (req, res) => {
+/*app.use('/', (req, res) => {
   res.status(404).send({message: `Something went wrong`});
+})*/
+
+app.get('/', (req, res) => {
+  res.send('hello world!');
 })
 
 app.listen(PORT, () => {
