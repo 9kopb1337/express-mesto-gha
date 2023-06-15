d7557aefa9d433dd3a7d450687d2a31efaea861f
Nikita <OvchinnikovNE1995@yandex.ru>
2023-06-15 17:24:04 +0300

    fix

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const router = require('./routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+  req.user = {
+    _id: '647f6834ca5b012549193198'
+  }
+  next();
+})
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(router);
 app.use('/', (req, res) => {
